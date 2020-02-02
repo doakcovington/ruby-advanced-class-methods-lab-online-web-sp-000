88b349153b505f3bdd81525135ab540b0d8fd5c3
Doak Covington <doakcovington@protonmail.com>
2020-02-02 23:59:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    song_array = filename.split(/[-.]/)
+    song_array = filename.split("-")
     song_array
     #song = self.new
     #song.name = song_array[1]
