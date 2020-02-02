bc639f2329881c7c026181c863e496f3150fdb10
Doak Covington <doakcovington@protonmail.com>
2020-02-02 22:42:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,7 @@ class Song
   def self.find_or_create_by_name(name)
     if self.name == name
       self.find_by_name(name)
+      name
     else
       self.create_by_name(name)
     end#end of if statement
